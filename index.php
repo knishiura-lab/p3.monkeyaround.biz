0cc2b02079b3d7bb97c1c92f219b9e76679e54d1
guang.long <gwong.long.2013@gmail.com>
2013-11-11 23:46:32 -0500

    fix timer

--- index.php
+++ index.php
@@ -14,22 +14,29 @@
   </head>
 
    <body>	
-      <h2> Find the matching images</h2>
+      <div class='title'> Find the Matching Images</div>
 
-      <h3>you will be presented with 20 picitures loaded from the internet.  
-       After clicking 'Start Game' button, you have about 10 seconds to memorize the images locations. <br>       
-       To make it more interesting, you can load images of your choice by enter a word, like fish, ants etc.</h3>
+      <div class="instructions">You will be presented with 20 picitures found from the internet.  <br>
+       You have about 10 seconds to memorize the image locations after clicking 'Start Game' button;<br>
+       Your goal is to find the matching images as soon as possible! <br>
+       To make it more interesting, you can make it more  add images of your choice by entering a word, like fish, ants etc. and click 'Load' <br>
+       but there is no gaurantee that your images will be added because the program will shuffle images from a pool of images.
+       </div>
+
  
-      <label for="imageKeywords"> Please load images for this word:</label>      
+      <label for="imageKeywords"> Enter word:</label>      
       <input type="text" id="imageKeywords" name='imageKeywords'>    
-
-      <div class="timer" id="timer"> </div><br> 
-      <div class="status" id="status">Please wait... </div><br>        
+      <button class='button' id='loadUserImages'> Load </button><br>
+      <div class="status" id="status">Please wait... </div>
+      <div class="status" id="timer"> </div>
+      <div class="status" id="clickCount"> </div> 
+      <div class="status" id="matchCount"> </div>
+      
       <button class='button' style="display:none" id="populate"> Populate Images</button>  
       <button class='button' style="display:none" id="hideImages"> Hide Images</button> 
       
       <button class='button' id="startGame"> Start Game</button> 
-      <button class='button' id="showImages"> Cheat</button> <br>
+      <button class='button' id="cheat"> Cheat</button> <br>
 
       <div  class="box"  id="sample_box_0" name='boxName'></div>   <br>
 
--- js/p3.js
+++ js/p3.js
@@ -29,8 +29,10 @@ function shuffle(array) {
 var imageLocations = new Array();
 
 // pull images using google's api, and store the source into a global variable: imageLocations
-function  getWebImagesByKeyword(keyword){  
-	console.log("getWebImagesByKeyword "+keyword)	;
+function  getWebImagesByKeyword(keyword){ 
+  $("#status").html('Loading '+keyword+'...'); 
+	//console.log("getWebImagesByKeyword "+keyword)	;
+  //$("#status").html('Please wait while searching for '+keyword);
 	// This is the URL for Google Image Search that we'll make the Ajax call to
 	var google_url = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&imgsz=medium&rsz=8&q=' + 
 	    keyword + '&callback=?';	
@@ -50,8 +52,10 @@ function  getWebImagesByKeyword(keyword){
 			// So here our data set is images
 			// Essentially we're unpacking our images we got back from Google, store them into an array
 	        $.each(images, function(key, image) {	        
-            	imageLocations.push(image.url);
+            	imageLocations.unshift(image.url);
 	        });
+          //$("#status").html('Done...');
+          clearStatusIn(1000);
 	    }	   
 	});			
 }
@@ -71,7 +75,8 @@ function getWebImagesByKeywords() {
     getWebImagesByKeyword(defaultImageKeywords.pop());
     setTimeout('getWebImagesByKeywords()',1000);
   } else {
-    $("#status").html('');
+    $("#status").html('&nbsp;');
+    clearStatusIn(1000);
   }
 }
 
@@ -149,14 +154,13 @@ function shuffleImages(pairsOffImages){
 
 function areImagesShuffled(){
   return shuffledImages.length>0;
-}
- 
+} 
 
 // logic for hide all images
 function showImages() {
    var allBoxes = document.getElementsByName("boxName");
    for (var j =0; j<allBoxes.length; j++) { 
-      var id = document.getElementsByName("boxName")[j].getAttribute("id").replace("clonedbox_",'').replace('sample_box_','');                    
+      var id = allBoxes[j].getAttribute("id").replace("clonedbox_",'').replace('sample_box_','');
       var image = shuffledImages[id];
       //console.log("image"+id+": "+image);           
       allBoxes[j].style.backgroundImage ='url('+image+")";       
@@ -172,6 +176,20 @@ function hideImages() {
       allBoxes[j].style.backgroundImage ='';       
    }
 }
+
+var matchedImageIdArray= new Array();
+
+// hide only unmatched images
+function hideUnmatchedImages(){
+   var allBoxes = document.getElementsByName("boxName");
+   for (var j =0; j<allBoxes.length; j++) {
+      var id = allBoxes[j].getAttribute("id");  
+      if (!isElementInArray(id,matchedImageIdArray)) { // no matched, hide it!
+        allBoxes[j].style.backgroundImage ='';   
+      }
+   }
+}
+
 // how many times user clicked the images on the page
 var numberOfClicks = 0;
 
@@ -184,14 +202,20 @@ var idClickedOnOddTime ='';
 // otherwise, not match!
 // program ignores the sequential clicks on the same image. 
 // considered user might have clicked it by mistake
-function manageUserClick(object) {        
+function manageUserClick(object) { 
+
     var idOfObjectClicked = object.attr('id');
+
     // only execute when using clicked a different box,
     // just in case user clicked the same one by mistake
-    if (idClickedOnOddTime != idOfObjectClicked) {  
+    if (idClickedOnOddTime != idOfObjectClicked && 
+        !isElementInArray(idOfObjectClicked,matchedImageIdArray)) {  
 
         showImage(idOfObjectClicked); 
         numberOfClicks +=1;
+
+        $('#clickCount').html('Total Clicks: '+numberOfClicks);  
+
         // if clicked odd time, simply record it
         if (numberOfClicks%2 ==1) {           
            idClickedOnOddTime = idOfObjectClicked;
@@ -200,10 +224,12 @@ function manageUserClick(object) {
         // if clicked even time, we need to compare to see if matched
         else  {            
            var matched =matchOddClick(idOfObjectClicked);
-           // hide it after 1 second if they do not match!
+           // hide it after 0.5 second if they do not match!
            if (!matched) {
-              setTimeout('hideImage("'+idOfObjectClicked+'")',1000);
-              setTimeout('hideImage("'+idClickedOnOddTime+'")',1000);
+              setTimeout('hideImage("'+idOfObjectClicked+'")',500);
+              setTimeout('hideImage("'+idClickedOnOddTime+'")',500);
+           } else {// record the images matched., so they are not clickable, not hide-able
+              recordMatchingId(idClickedOnOddTime,idOfObjectClicked);
            }
            processyMatchedStatus(matched);
         }
@@ -211,26 +237,34 @@ function manageUserClick(object) {
 
 }
 
+function recordMatchingId(idOfOddClick, idOfEvenClick){  
+  matchedImageIdArray.push(idOfOddClick);  
+  matchedImageIdArray.push(idOfEvenClick);
+}
+
 var matchedCount =0;
 // it displays the status based on matched or not.
 // record the total matchCount;
-function processyMatchedStatus(matched) {
-   $('#status').css('color','red') ;
-   if (matched) {
-      matchedCount +=1;
-      $('#status').html('Total matched images:'+(matchedCount*2) +". Congrats, you have a good memory!") ;      
-   } else {
-      $('#status').html('Total matched images:'+(matchedCount*2) +". Sorry, try again!") ;      
-   }
+function processyMatchedStatus(matched) {  
+  if (matched) {
+    matchedCount +=1;      
+    $('#status').html("Congratulations! You have a good memory!") ;        
+  } else {
+    $('#status').html("Sorry, try again!");      
+  }
 
-   if (matchedCount ==10) {
-      gameFinished = true;
-      var finishingTime = (new Date()).getTime();
-      var percentage = Math.round(20*1000/numberOfClicks)/10;
-      var finalMessage = 'Your finishing time is: '+ (finishingTime - startingTime)/1000 +" s!";
-      finalMessage += percentage+"% of your clicks are good\n";
-      $('#status').html('Congrats,You have found all of the matching images!\n'+ finalMessage);      
-    }
+  clearStatusIn(2000);
+
+  if (matchedCount == 10) {
+    gameFinished = true;
+    var finishingTime = (new Date()).getTime();
+    var percentage = Math.round(20*1000/numberOfClicks)/10;
+    var finalMessage = 'Your finishing time is: '+ (finishingTime - startingTime)/1000 +" s!";
+    finalMessage += "and "+percentage+"% of your clicks produced positive results\n";
+    $('#status').html('Congratulations!You have found all of the matching images!\n'+ finalMessage);      
+  }
+
+  $('#matchCount').html('Total matches: '+(matchedCount*2));  
   
 }
 
@@ -256,59 +290,68 @@ function hideImage(elementId) {
     document.getElementById(elementId).style.backgroundImage='';
 }
 
-function startTiming(){
+function timingUser(){
   var now = (new Date()).getTime(); 
   var secondsElapsed = Math.round((now-startingTime)/1000);
-  $('#timer').html('time elapsed: '+secondsElapsed+" s");
+  $('#timer').html('Time elapsed: '+secondsElapsed+" s");
   if (!gameFinished){
-      setTimeout('startTiming()',1000);  
+      setTimeout('timingUser()',1000);  
   }  
 }
  
-
- $('#imageKeywords').change(function(){
-     $("#status").html('');
-     var keywordEntered = $.trim($(this).val()); 
-     console.log('keywordEntered '+keywordEntered);
-     var defaultArray = keywordsUsed.split(',');
-
-     if (isElementInArray(keywordEntered, defaultArray) || isElementInArray(keywordEntered+"s", defaultArray) ) {
-         $("#status").html("The word you entered is being used by this program, please type a different word");
-         $("#status").css('color','red');
-     } else {
-        keywordsUsed += ','+keywordEntered;
-        getWebImagesByKeyword(keywordEntered);
-
-     }
- });
-
-// logic for populate images, 
-$('#populate').click (function() {
-  shuffleImages(10);
-  cloneBoxes();
-  showImages();
+$('#loadUserImages').click(function(){
+  var keyword = $.trim($('#imageKeywords').val());  
+  var valid= validateUserKeyword();
+  if (valid) {
+    $("#status").html('Please wait whiling loading ...'+keyword);
+    keywordsUsed += ','+keyword;
+    getWebImagesByKeyword(keyword);
+  }
 });
 
- 
-$('#showImages').click (function() {
+// check if word user entered is valid
+function validateUserKeyword(){
+  var valid = true;
+  $("#status").html('&nbsp;');
+  var keyword = $.trim($('#imageKeywords').val());
+  var defaultArray = keywordsUsed.split(',');
+  if (isElementInArray(keyword, defaultArray) || isElementInArray(keyword+"s", defaultArray) ) {
+    $("#status").html("The word you entered is being used by this program, please type a different word"); 
+    clearStatusIn(2000);
+    valid = false;
+  }
+
+  return valid;
+}
+
+
+// when user clicks cheat button, it displays for 1 sec and then fade out 
+$('#cheat').click (function() {
    showImages();
+   setTimeout('hideUnmatchedImages()',1000);
 });
-
-// logic for hide images
-$('#hideImages').click (function() {
-   hideImages();
- });
 
 var startingTime = (new Date()).getTime();
 var gameFinished = false;
-$("#startGame").click(function (){
- 
-   shuffleImages(10);
-   cloneBoxes();
-   showImages();
 
-   // give user 10 seconds to remember the game
-   fadeOutImages(12*1000);
+$("#startGame").click(function (){
+  // init variable
+  matchedImageIdArray = new Array();
+  matchCount =0;
+  clickCount =0;
+
+  // clear all messages
+  $("#status").html('&nbsp;');
+  $("#matchCount").html('&nbsp;');
+  $("#clickCount").html('&nbsp;');
+  $("#timer").html('&nbsp;');
+
+  shuffleImages(10);
+  cloneBoxes();
+  showImages();
+  
+  // give user 10 ~12 seconds to remember the game, and then fade out 
+  fadeOutImages(12*1000);
 });
 
 function fadeOutImages(timeToFadeOut){
@@ -316,44 +359,55 @@ function fadeOutImages(timeToFadeOut){
   $('[name="boxName"]').fadeOut(timeToFadeOut,function(){
     hideImages();
   })
-  $('[name="boxName"]').fadeIn('fast', function(){startGame(); });
+  $('[name="boxName"]').fadeIn('fast', function(){startTimingUser(); });
 }
 
-function startGame(){
+function startTimingUser(){
   startingTime = (new Date()).getTime(); 
   gameFinished = false;
-  startTiming(); 
+  timingUser(); 
 }
 
 var countDownFinished = false;
 var countDownStartingTime =0;
 
 function startCountingDown(){
-  countDownFinished = false;
-  console.log('starting');  
-  countDownStartingTime = (new Date()).getTime();
-  console.log('started');
+  countDownFinished = false;   
+  countDownStartingTime = (new Date()).getTime(); 
   countingDownTime();
 }
 
 function countingDownTime(){
-  $("#timer").html('');
+  $("#timer").html('&nbsp;');
   if (!countDownFinished) {
       var rightNow = (new Date()).getTime();
       var timeElapsed = rightNow - countDownStartingTime;
       var timeLeft = 10*1000-timeElapsed;
-      if (timeLeft >=1000) {
+      if (timeLeft >=1500) {
         $("#timer").html('Game will start in '+ timeLeft+' ms!');
         setTimeout('countingDownTime()',300);
       } else {
         countDownFinished = true;
-        $("#timer").html('');
+        $("#timer").html('&nbsp;');
       }
       
   }  
 }
 
-var numberOfClicks = 0;
-var previousClickedId;
+// wait a little bit, then clears the status message
+function clearStatusIn(millisToWait){
+  setTimeout('document.getElementById("status").innerHTML="&nbsp;"',millisToWait);
+}
 
+// the following 2 events will not be fired, the buttons are hidden
+// logic for populate images, the button is hidden, the 
+$('#populate').click (function() {
+  shuffleImages(10);
+  cloneBoxes();
+  showImages();
+});
 
+// logic for hide images
+$('#hideImages').click (function() {
+   hideImages();
+ });
