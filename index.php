e5b394db57a1e24b8cbe0ac69fd2b7abf839bcdd
kazuki <k-nishiura>
2014-01-17 21:32:32 +0900

    Tweak css/js path

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
 
     <!-- Common CSS/JSS -->
-    <link rel="stylesheet" href="/css/p3.css" type="text/css">
+    <link rel="stylesheet" href="css/p3.css" type="text/css">
     <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" /> 
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
@@ -72,7 +72,7 @@
     
     <br> <br>
 
-    <script type="text/javascript" src="/js/p3.js"></script>
+    <script type="text/javascript" src="js/p3.js"></script>
 
   </body>
 
